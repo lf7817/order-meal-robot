5aca3673c986c5f748f89a6fa87437d7b239238e
lf7817 <535536456@qq.com>
2021-11-09 10:06:15 +0800

    修改端口

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(80);
+  // 获取配置文件service
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('app.port'), '0.0.0.0');
 }
 bootstrap();
