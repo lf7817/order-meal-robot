65d84bfe9ac50373e5233e4cd2aa690f8210c78f
lf7817 <535536456@qq.com>
2021-11-09 15:22:46 +0800

    增加模拟数据

---
+++ src/modules/order-meal/mock.ts
@@ -0,0 +1,156 @@
+export const list = [
+  {
+    skuName: '晚餐140 安温客东山老鹅套餐',
+    productName: '晚餐140',
+    itemType: 1,
+    sellPoint: '',
+    pid: '45289749',
+    id: 'g60302752',
+    img: 'https://img.1000.com/qm-a-img/prod/1701679/b9b3528b069bae5705e921d9449d3358.JPG',
+    price: 15,
+    adminId: 'A4556225',
+    shopId: 'A1701679',
+    chainType: 'join',
+    stock: 93,
+    marketable: 1,
+    saleChannels: [1, 2],
+    skuList: [
+      {
+        skuId: 'g60302752',
+        spuId: '45289749',
+        adminId: 'A1701679',
+        skuName: '晚餐140 安温客东山老鹅套餐',
+        salePoint: '',
+        images: [
+          'https://img.1000.com/qm-a-img/prod/1701679/b9b3528b069bae5705e921d9449d3358.JPG',
+        ],
+        saleCount: 0,
+        stock: {
+          stock: 93,
+        },
+        oversold: 0,
+        salePrice: 15,
+        shopId: 'A1701679',
+        specs: [
+          {
+            specialPropName: '菜名',
+            specialPropId: 11921314,
+            specialValName: '安温客东山老鹅套餐',
+            specialValId: '34251799',
+          },
+        ],
+        unit: '件',
+        skuUnits: [
+          {
+            unitId: 'u27841174',
+            unit: '件',
+            mainUnit: true,
+            conversionNum: 1,
+            orderAllowable: false,
+            purchaseAllowable: false,
+            price: 15,
+            barCodes: [],
+            sort: 0,
+          },
+        ],
+        containerUnit: '',
+        addTime: '2021-11-09T12:46:36+0800',
+        saleChannels: [1, 2],
+        cats: [
+          {
+            name: 'b2c',
+            id: 'b2c',
+            childs: [
+              {
+                name: '晚餐',
+                id: '1005',
+                childs: [],
+              },
+            ],
+          },
+        ],
+        barCode: '',
+        productName: '晚餐140',
+        self: true,
+      },
+      {
+        skuId: 'g60302753',
+        spuId: '45289749',
+        adminId: 'A1701679',
+        skuName: '晚餐140 安温客水饺',
+        salePoint: '',
+        images: [
+          'https://img.1000.com/qm-a-img/prod/1701679/b9b3528b069bae5705e921d9449d3358.JPG',
+        ],
+        saleCount: 0,
+        stock: {
+          stock: 94,
+        },
+        oversold: 0,
+        salePrice: 15,
+        shopId: 'A1701679',
+        specs: [
+          {
+            specialPropName: '菜名',
+            specialPropId: 11921314,
+            specialValName: '安温客水饺',
+            specialValId: '34251800',
+          },
+        ],
+        unit: '件',
+        skuUnits: [
+          {
+            unitId: 'u27841175',
+            unit: '件',
+            mainUnit: true,
+            conversionNum: 1,
+            orderAllowable: false,
+            purchaseAllowable: false,
+            price: 15,
+            barCodes: [],
+            sort: 0,
+          },
+        ],
+        containerUnit: '',
+        addTime: '2021-11-09T12:46:36+0800',
+        saleChannels: [1, 2],
+        cats: [
+          {
+            name: 'b2c',
+            id: 'b2c',
+            childs: [
+              {
+                name: '晚餐',
+                id: '1005',
+                childs: [],
+              },
+            ],
+          },
+        ],
+        barCode: '',
+        productName: '晚餐140',
+        self: true,
+      },
+    ],
+    specs: [
+      {
+        specialPropId: '11921314',
+        specialPropName: '菜名',
+        specialValues: [
+          {
+            specialValId: '34251799',
+            specialValName: '安温客东山老鹅套餐',
+          },
+          {
+            specialValId: '34251800',
+            specialValName: '安温客水饺',
+          },
+        ],
+      },
+    ],
+    saleCount: 0,
+    skuLength: 2,
+    saleStartTime: '',
+    saleEndTime: '',
+  },
+];
