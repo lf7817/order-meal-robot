c953d4b634a10e3cb71697372081707fbcdc1123
lf7817 <535536456@qq.com>
2021-11-09 14:05:36 +0800

    添加cron.yaml

--- src/tasks/task.service.ts
+++ src/tasks/task.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { Cron, Interval, CronExpression } from '@nestjs/schedule';
+import { Cron } from '@nestjs/schedule';
 import { OrderMealService } from 'src/modules/order-meal/order-meal.service';
 
 @Injectable()
 export class TasksService {
   constructor(private readonly orderMealService: OrderMealService) {}
 
-  @Cron('0 30 13,14 * * ')
+  @Cron('0 0 13,14 * *')
   pushOrderMealMessage() {
     this.orderMealService.pushOrderMealMessage();
   }
